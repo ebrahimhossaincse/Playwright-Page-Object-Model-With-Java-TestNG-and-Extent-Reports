c7b00e77a32690cdae68fca4b2f2f427bd110ac7
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-19 03:57:29 -0400

    add register class

--- src/test/java/ebrahim/hossain/sqa/tests/LoginTest.java
+++ src/test/java/ebrahim/hossain/sqa/tests/LoginTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import ebrahim.hossain.sqa.basedriver.BaseDriver;
+import ebrahim.hossain.sqa.pages.LoginPage;
 import ebrahim.hossain.sqa.pages.RegisterPage;
 import ebrahim.hossain.sqa.utilities.ExtentFactory;
 
@@ -30,13 +31,17 @@ public class LoginTest extends BaseDriver {
 	}
 
 	@Test(priority = 0)
-	public void loginTest() throws IOException {
-		childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>LOGIN</b></p>");
-//		LoginPage loginPage = new LoginPage(childTest, page);
-//		loginPage.login();
-		
+	public void registrationTest() throws IOException {
+		childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>REGISTRATION</b></p>");	
 		RegisterPage register = new RegisterPage(childTest, page);
 		register.login();
+	}
+	
+	@Test(dependsOnMethods = {"registrationTest"})
+	public void loginTest() throws IOException {
+		childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>LOGIN</b></p>");
+		LoginPage loginPage = new LoginPage(childTest, page);
+		loginPage.login();
 	}
 
 	@AfterClass
