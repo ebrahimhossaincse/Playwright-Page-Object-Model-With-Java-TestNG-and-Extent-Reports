072e7939b36e306254f81f6ec9dfdacc21f324d3
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-09-21 16:31:14 -0400

    update login page

--- src/test/java/ebrahim/hossain/sqa/pages/LoginPage.java
+++ src/test/java/ebrahim/hossain/sqa/pages/LoginPage.java
@@ -1,107 +1,163 @@
 package ebrahim.hossain.sqa.pages;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
-import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.ElementHandle;
 import com.microsoft.playwright.Page;
 import ebrahim.hossain.sqa.utilities.CommonMethods;
 
 public class LoginPage extends CommonMethods {
-
+	// Define a Page object that represents the browser page in Playwright
 	Page page;
+
+	// Define an ExtentTest object for logging and reporting test results using
+	// ExtentReports
 	ExtentTest test;
 
-	private Locator email;
-	private Locator password;
-	private Locator login_button;
+	// Define ElementHandle objects for the username, password, and login button
+	// elements
+	private ElementHandle username;
+	private ElementHandle password;
+	private ElementHandle login_button;
 
-	public LoginPage(ExtentTest test, Page page) {
+	// Constructor to initialize the page and test objects, and locate the login
+	// elements
+	public LoginPage(Page page, ExtentTest test) {
+		// Initialize the Page object from the parameter
 		this.page = page;
+		// Initialize the ExtentTest object from the parameter
 		this.test = test;
 
-		this.email = page.locator("//input[@id='email']");
-		this.password = page.locator("//input[@id='password']");
-		this.login_button = page.locator("//input[@value='Login']");
+		// Locate the username input field using an XPath selector
+		this.username = page.querySelector("//input[@name='username']");
+		// Locate the password input field using an XPath selector
+		this.password = page.querySelector("//input[@name='password']");
+		// Locate the login button using an XPath selector
+		this.login_button = page.querySelector("//button[@type='submit']");
 	}
 
-	public Locator getEmail() {
-		return email;
+	// Getter method for the username ElementHandle
+	public ElementHandle getUsername() {
+		return username;
 	}
 
-	public void setEmail(Locator email) {
-		this.email = email;
+	// Setter method to update the username ElementHandle
+	public void setUsername(ElementHandle username) {
+		this.username = username;
 	}
 
-	public Locator getPassword() {
+	// Getter method for the password ElementHandle
+	public ElementHandle getPassword() {
 		return password;
 	}
 
-	public void setPassword(Locator password) {
+	// Setter method to update the password ElementHandle
+	public void setPassword(ElementHandle password) {
 		this.password = password;
 	}
 
-	public Locator getLogin_button() {
+	// Getter method for the login button ElementHandle
+	public ElementHandle getLogin_button() {
 		return login_button;
 	}
 
-	public void setLogin_button(Locator login_button) {
+	// Setter method to update the login button ElementHandle
+	public void setLogin_button(ElementHandle login_button) {
 		this.login_button = login_button;
 	}
 
+	// Method to log a success message with ExtentReports
 	public void handlePass(String message) {
+		// Log a message with green color indicating a pass
 		test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>" + message + "</b></p>");
 	}
 
+	// Method to log a success message and capture a screenshot
 	public void handlePassWithScreenshot(String message, String screenshotName) {
+		// Log a message with green color and include bold formatting
 		test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>" + message + "</b></p>");
+		// Capture a full-page screenshot and save it to the specified path
 		page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("./screenshots/" + screenshotName + ".png"))
 				.setFullPage(true));
-		String dest = System.getProperty("user.dir") + "/screenshots/" + "" + screenshotName + ".png";
+		// Build the full path for the screenshot file
+		String dest = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+		// Attach the screenshot to the test report
 		test.pass(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
 	}
 
+	// Method to log a failure message, capture a screenshot, and log an exception
 	public void handleFail(String message, String screenshotName) {
+		// Log a failure message with red color indicating an error
 		test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>" + message + "</b></p>");
+		// Create an exception and log it in the report
 		Throwable t = new InterruptedException("Exception");
 		test.fail(t);
+		// Capture a full-page screenshot in case of failure and save it to the
+		// specified path
 		page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("./screenshots/" + screenshotName + ".png"))
 				.setFullPage(true));
-		String dest = System.getProperty("user.dir") + "/screenshots/" + "" + screenshotName + ".png";
+		// Build the full path for the screenshot file
+		String dest = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+		// Attach the screenshot to the failure report
 		test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
-		closePlaywright();
 	}
 
-	public void login() throws IOException {
+	// Method to perform the login operation
+	public void login() throws InterruptedException {
+		// Log the start of the login process
+		test.info("Login Process");
 		try {
-			test.info("Login Page");
-			if (email.isVisible()) {
-				test.info("Please Enter your email address");
-				email.fill("ebrahim@noemail.com");
-				page.waitForTimeout(1000);
-				handlePass("You have successfully entered your Email");
-				if (password.isVisible()) {
-					test.info("Please Enter your password");
-					password.fill("test@123");
-					page.waitForTimeout(1000);
-					handlePass("You have successfully entered your Password");
-					if (login_button.isVisible()) {
-						login_button.click();
-						page.waitForTimeout(1000);
-						handlePassWithScreenshot("You have successfully clicked on the Login", "login_pass");
-					} else {
-						handleFail("Login Button was not locateable. Please check the error message",
-								"password_locator_fail");
+			// Check if the username field is visible
+			if (username.isVisible()) {
+				// Log an instruction to enter the username
+				test.info("Please enter your email address.");
+				// Fill the username field with the text "Admin"
+				username.fill("Admin");
+				// Wait for 2 seconds to simulate a delay
+				Thread.sleep(2000);
+				// Log the success of entering the username
+				handlePass("You had successfully entered your username");
+
+				try {
+					// Check if the password field is visible
+					if (password.isVisible()) {
+						// Log an instruction to enter the password
+						test.info("Please enter your password.");
+						// Fill the password field with the text "admin123"
+						password.fill("admin123");
+						// Wait for 2 seconds to simulate a delay
+						Thread.sleep(2000);
+						// Log the success of entering the password
+						handlePass("You had successfully entered your password");
+
+						try {
+							// Check if the login button is visible
+							if (login_button.isVisible()) {
+								// Log an instruction to click the login button
+								test.info("Please click on the Login Button.");
+								// Click the login button
+								login_button.click();
+								// Wait for 5 seconds to simulate the login process
+								Thread.sleep(5000);
+								// Log the success of the login attempt and capture a screenshot
+								handlePassWithScreenshot("You had successfully logged in", "login_success");
+							}
+						} catch (Exception e) {
+							// Handle failure when login button is not found and capture a screenshot
+							handleFail("Login Button was not locateable. Please check the error message",
+									"login_button_fail");
+						}
 					}
-				} else {
-					handleFail("Password was not locateable. Please check the error message", "password_locator_fail");
+				} catch (Exception e) {
+					// Handle failure when password field is not found and capture a screenshot
+					handleFail("Password was not locateable. Please check the error message", "password_fail");
 				}
-			} else {
-				handleFail("Email Address was not locateable. Please check the error message.", "email_locator_fail");
 			}
 		} catch (Exception e) {
-			handleFail("An error occurred during login. Please check the error message.", "login_error");
+			// Handle failure when username field is not found and capture a screenshot
+			handleFail("User Name was not locateable. Please check the error message", "username_fail");
 		}
 	}
+
 }
--- src/test/java/ebrahim/hossain/sqa/tests/LoginTest.java
+++ src/test/java/ebrahim/hossain/sqa/tests/LoginTest.java
@@ -1,10 +1,11 @@
 package ebrahim.hossain.sqa.tests;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -13,33 +14,47 @@ import ebrahim.hossain.sqa.pages.LoginPage;
 import ebrahim.hossain.sqa.utilities.ExtentFactory;
 
 public class LoginTest extends BaseDriver {
-
+	
 	ExtentReports report;
 	ExtentTest parentTest;
 	ExtentTest childTest;
 
 	@BeforeClass
-	@Parameters({ "url", "browserName", "headless" })
-	public void openUrl(@Optional("https://www.tutorialspoint.com/selenium/practice/login.php") String url,
-			@Optional("chrome") String browserName, @Optional("false") String headless) throws InterruptedException {
+	public void openUrl(@Optional String url, @Optional String browserName, @Optional String headless) throws InterruptedException {
+		Properties properties = new Properties();
+		String env = System.getProperty("env", "dev");
+		String configFileName = String.format("config-%s.properties", env);
+
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName)) {
+			if (input == null) {
+				System.out.println("Sorry, unable to find " + configFileName);
+				return;
+			}
+			properties.load(input);
+			url = url != null ? url : properties.getProperty("url");
+			browserName = browserName != null ? browserName : properties.getProperty("browserName");
+			headless = headless != null ? headless : properties.getProperty("headless");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
 		report = ExtentFactory.getInstance();
-		parentTest = report.createTest("<p style=\"color:#FF6000; font-size:20px\"><b>LOGIN MODULE</b></p>")
+		parentTest = report.createTest("<p style=\"color:#FF6000; font-size:20px\"><b>SALES PORTAL - BOOKER</b></p>")
 				.assignAuthor("QA TEAM").assignDevice("Windows");
 		launchPlaywright(browserName, headless);
 		launchApplication(url);
 	}
-	
-	@Test(priority = 0)
-	public void loginTest() throws IOException {
-		childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>LOGIN</b></p>");
-		LoginPage loginPage = new LoginPage(childTest, page);
-		loginPage.login();
-	}
 
+	@Test
+	public void loginTest() throws IOException, InterruptedException {
+		childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>Login</b></p>");
+		LoginPage loginPage = new LoginPage(page, childTest);
+		loginPage.login();		
+	}
+	
 	@AfterClass
 	public void afterClass() {
 		closePlaywright();
 		report.flush();
 	}
-
 }
